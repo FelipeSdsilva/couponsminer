5b97a0f2cb65970407b4f3ead12ede0af342ffe6
Felipe Sousa <felipe.fps09@hotmail.com>
2024-08-23 17:35:55 -0300

    Feat: inicialização do projeto.

---
+++ src/main/java/com/felipesousa/cupomminer/CupomminerApplication.java
@@ -0,0 +1,13 @@
+package com.felipesousa.cupomminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupomminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupomminerApplication.class, args);
+	}
+
+}
