525763406fd89636a672d537302a3b1f3e495b31
Felipe Sousa <felipe.fps09@hotmail.com>
2024-08-24 19:19:05 -0300

    Feat: Incluindo annotations para melhorar documentação.

--- src/main/java/com/felipesousa/cupomminer/controllers/CouponController.java
+++ src/main/java/com/felipesousa/cupomminer/controllers/CouponController.java
@@ -4,6 +4,8 @@ package com.felipesousa.cupomminer.controllers;
 import com.felipesousa.cupomminer.dto.CouponDTO;
 import com.felipesousa.cupomminer.dto.CouponMinDTO;
 import com.felipesousa.cupomminer.services.CouponService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,14 @@ import static org.springframework.web.servlet.support.ServletUriComponentsBuilde
 
 @RestController
 @RequestMapping(value = "/coupons")
+@Tag(name = "Coupon")
 public class CouponController {
 
     @Autowired
     private CouponService couponService;
 
     @PostMapping
+    @Operation(description = "This endpoint included new coupon in the system if valid")
     public ResponseEntity<CouponMinDTO> postNewCoupon(@Valid @RequestBody CouponDTO couponDTO) {
         CouponMinDTO minDTO = couponService.saveCoupon(couponDTO);
         var uri = fromCurrentRequest().path("/{id}").buildAndExpand(minDTO.getCouponId()).toUri();
