fa26836eda4fbf2603515a9580dfc490c410787b
Felipe Sousa <felipe.fps09@hotmail.com>
2024-08-26 18:32:18 -0300

    Fix: Implementando deserialização para listner.

--- src/main/java/com/felipesousa/cupomminer/config/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/felipesousa/cupomminer/config/rabbitmq/RabbitMQConfig.java
@@ -1,9 +1,9 @@
 package com.felipesousa.cupomminer.config.rabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.AllowedListDeserializingMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,5 +28,27 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverterJackson() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AllowedListDeserializingMessageConverter messageConverter() {
+        AllowedListDeserializingMessageConverter converter = new AllowedListDeserializingMessageConverter() {
+            @Override
+            public Object fromMessage(Message message) throws MessageConversionException {
+                return null;
+            }
+
+            @Override
+            protected Message createMessage(Object o, MessageProperties messageProperties) {
+                return null;
+            }
+        };
+        converter.addAllowedListPatterns("com.felipesousa.cupomminer.entities.Coupon");
+        return converter;
     }
 }
