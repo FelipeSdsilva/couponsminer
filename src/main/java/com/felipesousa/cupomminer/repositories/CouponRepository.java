d8d182d5599d03745decd9d0fffb8bf3f7c1192a
Felipe Sousa <felipe.fps09@hotmail.com>
2024-08-26 18:29:20 -0300

    Feat: Implementando consulta por codigo44 para poder atualizar via listner no service implementação para validação do EAN e verificação de preço por mock api.

--- src/main/java/com/felipesousa/cupomminer/repositories/CouponRepository.java
+++ src/main/java/com/felipesousa/cupomminer/repositories/CouponRepository.java
@@ -3,5 +3,9 @@ package com.felipesousa.cupomminer.repositories;
 import com.felipesousa.cupomminer.entities.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    Optional<Coupon> findByCode44(String code44);
 }
--- src/main/java/com/felipesousa/cupomminer/services/CouponService.java
+++ src/main/java/com/felipesousa/cupomminer/services/CouponService.java
@@ -4,15 +4,20 @@ import com.felipesousa.cupomminer.config.rabbitmq.RabbitMQConfig;
 import com.felipesousa.cupomminer.dto.CouponDTO;
 import com.felipesousa.cupomminer.dto.CouponMinDTO;
 import com.felipesousa.cupomminer.dto.ProductDTO;
+import com.felipesousa.cupomminer.dto.ProductResponseDTO;
 import com.felipesousa.cupomminer.entities.Coupon;
 import com.felipesousa.cupomminer.exceptions.TotalValueException;
 import com.felipesousa.cupomminer.repositories.CouponRepository;
+import com.felipesousa.cupomminer.requests.ProductRequest;
+import com.felipesousa.cupomminer.utils.DocFormat;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class CouponService {
@@ -24,16 +29,15 @@ public class CouponService {
     private RabbitTemplate rabbitTemplate;
 
     @Transactional
-    public CouponMinDTO saveCoupon(CouponDTO coupon) {
+    public CouponMinDTO saveCoupon(CouponDTO coupon) throws Exception {
 
         Coupon couponEntity = new Coupon();
         converterDtoInEntity(coupon, couponEntity);
 
         BigDecimal totalValue = new BigDecimal("0");
 
-        for (ProductDTO product : coupon.getProducts()) {
-            totalValue = totalValue.add(product.getUnitaryPrice().multiply(new BigDecimal(product.getQuantity())));
-        }
+        totalValue = validateProducts(coupon.getProducts(), totalValue);
+
         if (!totalValue.equals(coupon.getTotalValue()))
             throw new TotalValueException("The total value of the coupon does not match the sum of the product values!");
 
@@ -41,8 +45,35 @@ public class CouponService {
 
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, couponEntity);
 
-        return new CouponMinDTO(couponEntity.getId(), "", couponEntity.getPurchaseDate(), "");
+        String doc = DocFormat.formatDOc(couponEntity.getCompanyDocument());
+
+        return new CouponMinDTO(couponEntity.getId(), "Lucas Souza", couponEntity.getPurchaseDate(), doc);
     }
+
+    @Transactional
+    @RabbitListener(queues = "coupon-queue")
+    public void handleCouponUpdate(CouponDTO message) {
+        Coupon coupon = couponRepository.findByCode44(message.getCode44())
+                .orElseThrow(() -> new RuntimeException("Coupon not found"));
+        converterDtoInEntity(message, coupon);
+        couponRepository.save(coupon);
+    }
+
+    protected BigDecimal validateProducts(List<ProductDTO> products, BigDecimal totalValue) throws Exception {
+        for (ProductDTO product : products) {
+            totalValue = totalValue.add(product.getUnitaryPrice().multiply(new BigDecimal(product.getQuantity())));
+
+            ProductResponseDTO productResponse = ProductRequest.getProduct(product.getEan().toString());
+
+            if (productResponse == null ||
+                    product.getUnitaryPrice().compareTo(productResponse.getMinPrice()) < 0 ||
+                    product.getUnitaryPrice().compareTo(productResponse.getMaxPrice()) > 0) {
+                throw new IllegalArgumentException("Invalid product price for EAN: " + product.getEan());
+            }
+        }
+        return totalValue;
+    }
+
 
     private void converterDtoInEntity(CouponDTO coupon, Coupon couponEntity) {
         couponEntity.setCode44(coupon.getCode44());
